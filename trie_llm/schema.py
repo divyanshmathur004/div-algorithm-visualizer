c0b0ca014f19ea015457c5daf1fd9f477098de3b
Divyansh Mathur <127034148+divyanshmathur004@users.noreply.github.com>
2025-12-10 23:21:24 +0530

    Delete trie_llm directory

--- trie_llm/schema.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-
-#Schema for the ThemedWordList
-class ThemedWordList(BaseModel):
-    # Pydantic Word validation for the list 
-    words = List[str]
